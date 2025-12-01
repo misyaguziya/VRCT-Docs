a4ecfe7430fa35a6f4b3f8d4c28e83415ab41010
misyaguziya <53165965+misyaguziya@users.noreply.github.com>
2025-12-01 16:00:10 +0900

    ドキュメントの「メッセージを受信する」ガイドを更新し、翻訳機能の有効化手順と受信メッセージの確認方法を追加しました。また、関連する画像を追加しました。さらに、ホームページのヘッダーにコメントを追加しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary-base', styles.heroBanner)}>
+    {/* <header className={clsx('hero hero--primary', styles.heroBanner)}> */}
       <div className="container">
         <div>
             <img src="img/vrct_logo_white.png" alt="VRCT Logo"/>
